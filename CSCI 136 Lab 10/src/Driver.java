6ec834a545f44ec48130916da78a620fabd6b58b
ncrep <ncrep@DESKTOP-2ULBP3G>
2018-12-01 17:13:06 -0700

    Push

---
+++ CSCI 136 Lab 10/src/Driver.java
@@ -0,0 +1,101 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Driver {
+	static File bitterroot;
+	static File coyote;
+	static Scanner myFileScanner;
+	static ArrayList<String> bitterrootArray = new ArrayList<String>();
+	static Scanner myCoyoteScanner;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		bitterroot = new File("./src/Coyote and the Bitterroot Valley.txt");
+		coyote = new File("./src/Coyote.txt");
+		myCoyoteScanner = new Scanner(coyote);
+		myFileScanner = new Scanner(bitterroot);
+		readInBitterroot(myFileScanner);
+		readInCoyote(myCoyoteScanner);
+		Sorting mySort = new Sorting();
+		String[] array = new String[2084];
+		for(int i = 0; i<bitterrootArray.size(); i++) {
+			array[i] = bitterrootArray.get(i);
+		}
+		mySort.selectionSort(array);
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
+		Scanner msc = new Scanner(System.in);
+		System.out.println("Would you like to search for a certian words frequency? Y/N");
+		String ans = msc.nextLine();
+		while (ans.equals("Y")) {
+			System.out.println("What word would you like to search for?");
+			String word = msc.nextLine();
+			word = word.toLowerCase();
+			int count = search(bitterrootArray, word);
+			System.out.println(word + ": " + count);
+			System.out.println("Would you like to search for another word? Y/N");
+			ans = msc.nextLine();
+		}
+
+	}
+
+	public static void readInBitterroot(Scanner myFileScan) throws FileNotFoundException {
+		if (myFileScan.hasNext()) {
+			String word = myFileScan.next();
+			word = word.toLowerCase();
+			word = word.replaceAll("\\p{Punct}", "");
+			word.trim();
+			if (word.equals("the") || word.equals("and") || word.equals("it") || word.equals("a") || word.equals("on")
+					|| word.equals("in")) {
+				word = myFileScan.next();
+				word = word.toLowerCase();
+				word = word.replaceAll("\\p{Punct}", "");
+				word.trim();
+				bitterrootArray.add(word);
+				readInBitterroot(myFileScan);
+			} else {
+				bitterrootArray.add(word);
+				readInBitterroot(myFileScan);
+			}
+		}
+
+	}
+
+	public static void readInCoyote(Scanner myCoyoteScan) throws FileNotFoundException {
+		if (myCoyoteScan.hasNext()) {
+			String cWord = myCoyoteScan.next();
+			cWord = cWord.toLowerCase();
+			cWord = cWord.replaceAll("\\p{Punct}", "");
+			cWord.trim();
+			if (cWord.equals("the") || cWord.equals("and") || cWord.equals("it") || cWord.equals("a")
+					|| cWord.equals("on") || cWord.equals("in")) {
+				cWord = myCoyoteScan.next();
+				cWord = cWord.toLowerCase();
+				cWord = cWord.replaceAll("\\p{Punct}", "");
+				cWord.trim();
+				bitterrootArray.add(cWord);
+				readInCoyote(myCoyoteScan);
+			} else {
+				bitterrootArray.add(cWord);
+				readInCoyote(myCoyoteScan);
+			}
+		}
+	}
+
+	public static int search(ArrayList<String> array, String target) {
+		int counter = 0;
+		for (int i = 0; i < array.size(); i++) {
+			if (target.equals(array.get(i))) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+}
